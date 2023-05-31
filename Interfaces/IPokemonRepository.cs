c687945e9988659c8af31947ebd1d2093b801b75
Adnan <machack2291@gmail.com>
2023-05-31 12:21:48 +0530

    Sample First Commit

---
+++ Interfaces/IPokemonRepository.cs
@@ -0,0 +1,19 @@
+﻿using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Interfaces
+{
+    public interface IPokemonRepository
+    {
+        ICollection<Pokemon> GetPokemons();
+
+        Pokemon GetPokemon(int id);
+
+        Pokemon GetPokemon(String name);
+
+        decimal GetPokemkonRating(int pokeId);
+
+        bool PokemonExists(int pokeId);
+
+
+    }
+}
